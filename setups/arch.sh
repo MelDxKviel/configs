3d26248b7536b1e3422b607d0c846a30335a12fa
meldxkviel <mega1hack52@gmail.com>
2022-08-31 12:35:35 +0700

    Add game stuff installation

--- setups/arch.sh
+++ setups/arch.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 
+# Update packages
 sudo pacman -Syu
 
+# Install apps
 sudo pacman -S bat btop curl dust exa docker docker-compose flatpak htop keepassxc neofetch neovim networkmanager ranger telegram-desktop firefox tmux tree vim zsh okular gparted python python-pip wget
 
+# Install game stuff
+read -r -p "Install game stuff? [y/N] " response
+case "$response" in
+    [yY][eE][sS]|[yY]) 
+        sudo pacman -S steam discord
+        ;;
+    *)
+        ;;
+esac
+
+# Install python packages
 source ./python.sh
 
+# Start common script
 source ./common.sh
+
