ebbfa4dfaa9da391091f68188bd780b732d98136
meldxkviel <mega1hack52@gmail.com>
2022-08-31 12:34:42 +0700

    Add vim-plug installation

--- setups/common.sh
+++ setups/common.sh
@@ -12,3 +12,6 @@ sudo mkdir /usr/share/fonts/truetype/ && cp *.ttf /usr/share/fonts/truetype/
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
+# Install vim-plug
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
